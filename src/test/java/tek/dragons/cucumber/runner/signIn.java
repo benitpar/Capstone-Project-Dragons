594ba74b6aa3fa9c70125f82c4e21384246fc748
Louis Benitez <louisben1998@gmail.com>
2023-05-30 23:22:36 -0400

    Final2

--- src/test/java/tek/dragons/cucumber/runner/signIn.java
+++ src/test/java/tek/dragons/cucumber/runner/signIn.java
@@ -8,7 +8,7 @@ import static io.cucumber.junit.CucumberOptions.SnippetType.CAMELCASE;
 //@RunWith is a JUnit Annotation
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		tags = "@signIn",
+		tags = "@home",
 		features = ("classpath:features"),
 		glue = "tek.dragons.cucumber",
 		dryRun = false,
